753eb1676e22623a218a2828b04a65166c9281b8
Bogdan Dadu <dadu.bogdan@gmail.com>
2019-05-06 21:34:35 +0300

    Save(with unique ID)andDelete

--- public/javascripts/agenda.js
+++ public/javascripts/agenda.js
@@ -1,11 +1,13 @@
 var allPersons = [];
 
 var API_URL = {
-    ADD: 'users/add'
+    ADD: 'users/add',
+    DELETE: 'users/delete'
 };
 var API_METHOD = {
     //ADD:'GET'
-    ADD: 'POST'
+    ADD: 'POST',
+    DELETE: 'DELETE'
 }
 fetch('data/persons.json').then(function (r) {
     return r.json();
@@ -17,11 +19,14 @@ fetch('data/persons.json').then(function (r) {
 })
 function display(persons) {
     var list = persons.map(function (person) {
-        return `<tr>
+        return `<tr data-id="${person.id}">
                 <td>${person.firstName}</td>
                 <td>${person.lastName}</td>
                 <td>${person.phone}</td>
-                <td></td>
+                <td>
+                <a href="#" class="delete">&#10006;</a>
+                <a href="#" class="edit">&#9998;</a>
+            </td>
             </tr>`
     });
 
@@ -50,23 +55,24 @@ function submitNewPerson(firstName, lastName, phone) {
     fetch(API_URL.ADD, {
         method: API_METHOD.ADD,
         body: body,
-        headers:{
-            "Content-Type":"application/json"
+        headers: {
+            "Content-Type": "application/json"
         }
     }).then(function (r) {
         return r.json();
     }).then(function (status) {
         if (status.success) {
 
-            inlineAddPerson(firstName, lastName, phone);
+            inlineAddPerson(status.id, firstName, lastName, phone);
         } else {
             console.warn('not saved', status);
         }
     })
 }
 
-function inlineAddPerson(firstName, lastName, phone) {
+function inlineAddPerson(id, firstName, lastName, phone) {
     allPersons.push({
+        id,
         firstName: firstName,
         lastName: lastName,
         phone: phone
@@ -74,3 +80,54 @@ function inlineAddPerson(firstName, lastName, phone) {
 
     display(allPersons);
 }
+
+function inlineDeletePerson(id) {
+    console.warn('please refers', id);
+    allPersons = allPersons.filter(function (person) {
+        return person.id != id;
+    });
+    display(allPersons);
+}
+
+function deletePerson(id) {
+    var body = null;
+    if (API_METHOD.DELETE === 'DELETE') {
+        body = JSON.stringify({ id });
+    }
+
+    console.log('body', body);
+
+    fetch(API_URL.DELETE, {
+        method: API_METHOD.DELETE,
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (r) {
+        return r.json();
+    }).then(function (status) {
+        if (status.success) {
+
+            inlineDeletePerson(id);
+        } else {
+            console.warn('not removed', status);
+        }
+    })
+}
+
+
+
+function initEvents() {
+    const tbody = document.querySelector('#agenda tbody');
+    tbody.addEventListener('click', function (e) {
+        console.warn('click on tbody', e.target);
+        if (e.target.className == 'delete') {
+            const tr = e.target.parentNode.parentNode;
+            const id = tr.getAttribute('data-id');
+            console.warn('parent!', e.target.parentNode.parentNode);
+            deletePerson(id);
+        }
+    });
+}
+
+initEvents();
--- routes/users.js
+++ routes/users.js
@@ -3,33 +3,59 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
- res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
 });
 
 // http://localhost:3000/users/add
 // Save a new person
-router.post('/add', function(req, res, next) {
- var firstName = req.body.firstName;
- var lastName = req.body.lastName;
- var phone = req.body.phone;
- console.warn('add', firstName, lastName, phone);
- var persons = require('../public/data/persons.json');
+router.post('/add', function (req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var phone = req.body.phone;
+  console.warn('add', firstName, lastName, phone);
+  var persons = require('../public/data/persons.json');
 
- persons.push({
-   firstName,
-   lastName,
-   phone
- });
+   const id=new Date().getTime()
+  persons.push({
+    id,
+    firstName,
+    lastName,
+    phone
+  });
 
- var str = JSON.stringify(persons, null, 2);
- fs.writeFileSync('./public/data/persons.json', str);// se sterge in fiser ce a fost si se salveaza noile date
 
 
- res.json({
-   success: true,
-   message: 'TODO'
- });
+
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);// se sterge in fiser ce a fost si se salveaza noile date
+
+
+  res.json({
+    success: true,
+    id,
+    message: 'DONE'
+  });
+});
+router.delete('/delete', function (req, res, next) {
+  var id = req.body.id;
+
+  console.warn('remove person', id);
+  var persons = require('../public/data/persons.json');
+  var remainingPersons = persons.filter(function (person) {
+    return person.id != id;
+  });
+
+  console.log('remainingPersons: ', remainingPersons)
+
+  var str = JSON.stringify(remainingPersons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);// se sterge in fiser ce a fost si se salveaza noile date
+
+
+  res.json({
+    success: true,
+    message: 'TODO'
+  });
 });
 
 module.exports = router;
